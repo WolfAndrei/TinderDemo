2e410fa98087c68548f9708e439f35638e732425
Andrei Volkau <101.andrei.wolf@mail.ru>
2020-07-22 15:25:04 +0300

    Initial Commit

---
+++ TinderDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderDemo
+//
+//  Created by Andrei Volkau on 22.07.2020.
+//  Copyright © 2020 Andrei Volkau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
